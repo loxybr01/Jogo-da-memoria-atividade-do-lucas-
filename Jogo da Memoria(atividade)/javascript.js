513ebba1551c8e2ff825c0570e062d1d2adb9342
loxybr01 <117488811+loxybr01@users.noreply.github.com>
2023-02-28 20:04:21 -0300

    Add files via upload

---
+++ Jogo da Memoria(atividade)/javascript.js
@@ -0,0 +1,11 @@
+let cards = [
+    { img: "card1.png", selected: false },
+    { img: "card2.png", selected: false },
+    { img: "card3.png", selected: false },
+    { img: "card4.png", selected: false },
+    { img: "card1.png", selected: false },
+    { img: "card2.png", selected: false },
+    { img: "card3.png", selected: false },
+    { img: "card4.png", selected: false }
+  ];
+  
